0f92188a85801fa99f3d985d2062a23ec6488b44
Neepurna <neepurna@gmail.com>
2024-07-24 17:05:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import Experience from "./Experience";
-import Interface from "./Interface";
+import { OrbitControls } from "@react-three/drei";
+import { useEffect } from "react";
+
+function LogCameraPosition() {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'p') {
+        console.log(camera.position);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [camera]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [2, 1.5, 2.5], fov: 30 }} shadows>
+      <Canvas camera={{ position: [7.950236292734576, 2, 7.881748094799187], fov: 30, near: 0.1, far: 10000 }} shadows>
         <Experience />
+        <OrbitControls 
+          enablePan={false} 
+          enableZoom={false} 
+          target={[0, 0, 0]}
+          minDistance={0}
+          maxDistance={Infinity}
+        />
+        <LogCameraPosition />
       </Canvas>
-      {/* <Interface /> */}
     </>
   );
 }
